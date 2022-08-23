67c6f758a04203ede0608398bfedf358f68ede87
waranagi1973 <85052491+waranagi1973@users.noreply.github.com>
2022-08-23 09:25:58 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
 	
 })
+input.onGesture(Gesture.Shake, function () {
+    歩数計 += 1
+})
+let 歩数計 = 0
 music.setBuiltInSpeakerEnabled(true)
 basic.forever(function () {
     music.setTempo(input.soundLevel())
@@ -18,6 +22,9 @@ basic.forever(function () {
         basic.showString("" + (input.compassHeading()))
     } else if (input.logoIsPressed()) {
         basic.showString("    Now Vertion:1.0.1")
+    } else if (input.pinIsPressed(TouchPin.P0)) {
+        basic.showString("HOSU'UKEI")
+        basic.showString("" + (歩数計))
     } else {
         led.plotBarGraph(
         input.soundLevel(),
